ba450a25851b8e8d8e2c1824a80bdef44ac4516b
Ryan Earl <95715476+RyanWEarl5@users.noreply.github.com>
2021-12-10 13:35:43 -0600

    day 2 part 1 complete!
    fun goofing around with OOP

---
+++ Day 2/day2.py
@@ -0,0 +1,38 @@
+with open("day2-data.txt") as f:
+    commands = [i.strip() for i in f.readlines()]
+print("Day 2 Challenge Part 1\n")
+
+class Submarine:
+    
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.dir_count = 0
+    
+    def updatePos(self, command):
+        comm_dir = command.split(" ")
+        direction, dist = comm_dir[0], int(comm_dir[1])
+        
+        if direction == "forward":
+            self.xpos += dist
+        if direction == "up":
+            self.ypos += dist * -1
+        if direction == "down":
+            self.ypos += dist
+            
+        self.dir_count += 1
+            
+    def printPos(self):
+        if self.dir_count % 50 == 0:
+            print(f"X Position: {self.xpos}, Y Position: {self.ypos}\n")
+        
+    def finalAnswer(self):
+        print(self.xpos * self.ypos)
+        
+    
+sub = Submarine(0,0)
+for com in commands:
+    sub.updatePos(com)
+    sub.printPos()
+
+sub.finalAnswer()
